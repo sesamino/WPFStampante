4ca61060f77713bcb9dd0531b8454bc026e4f9ec
Almina Pajazitovska <127589975+sesamino@users.noreply.github.com>
2023-12-04 19:38:49 +0100

    Add files via upload

---
+++ pajazitovska.almina.4i.stampante/pajazitovska.almina.4i.stampante/Models/Pagina.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pajazitovska.almina._4i.stampante.Models
+{
+    public class Pagina
+    {
+        public int C { get; set; }
+        public int M { get; set; }
+        public int Y { get; set; }
+        public int B { get; set; }
+
+    }
+}
---
+++ pajazitovska.almina.4i.stampante/pajazitovska.almina.4i.stampante/Models/Stampante.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pajazitovska.almina._4i.stampante.Models
+{
+    public class Stampante
+    {
+        //serbatoi di colore
+        public int C { get; set; }
+        public int M { get; set; } 
+        public int Y{ get; set; }      
+        public int B { get; set; }
+
+        public int Fogli { get; set; }
+        //PRIMO COSTRUTTORE
+        public Stampante() { 
+            C = M = Y = B = 100;
+            Fogli = 200;
+        }
+        //metodo bool Stampa(Pagina p)
+        // torna false se non ci sono pagine o colori per stampare
+        public bool Stampa(Pagina p)
+        {                     
+            if( (Fogli>0) && 
+                (C >= p.C)&&
+                (M >= p.M) &&   
+                (Y >= p.Y) &&   
+                (B >= p.B) 
+                ){//controllare se è ciano o magenta o yellow o black (si può scrivere anche sulla stessa riga ma scrivendolo in questo modo riga per riga è più leggibile)
+
+                    C -= p.C;
+                    M -= p.M;
+                    Y -= p.Y;
+                    B -= p.B;
+                    Fogli--;
+
+                    return true;
+            }
+            return false;
+        }
+        public int StatoInchiostro(string colore) 
+        { 
+
+        }
+
+    }
+}
